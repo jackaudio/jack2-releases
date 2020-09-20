259fb8d93a2ddc0044ebe2ee4c23f7e42c5b92da
falkTX <falktx@falktx.com>
2020-09-20 17:19:23 +0100

    Try to fix CI for 20.04
    Signed-off-by: falkTX <falktx@falktx.com>

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,13 +6,11 @@ if [ "${TARGET}" = "macos" ]; then
     exit 0
 fi
 
-if [ "${TARGET}" = "win32" ]; then
-    sudo dpkg --add-architecture i386
-fi
-
 if [ "${TARGET}" = "win32" ] || [ "${TARGET}" = "win64" ]; then
     wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
     sudo apt-add-repository -y 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+    sudo dpkg --add-architecture i386
 fi
 
 sudo apt-get update -qq
+sudo apt-get install libc6 libc6:i386
