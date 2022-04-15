8a33a684da9e26d6a61d31c13ae1ea8a896d4cf2
falkTX <falktx@falktx.com>
2022-04-15 20:19:37 +0100

    Tag 1.9.21 release
    Signed-off-by: falkTX <falktx@falktx.com>

--- pack-jack2-version.sh
+++ pack-jack2-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export JACK2_VERSION=84d80c0a8c4f62839995ff96ad2b00d3ffed6211
+export JACK2_VERSION=1abd04edab2fef8e7b5e5197dd03034358a37078
 export QJACKCTL_VERSION=0.9.7
 
 set -e
@@ -26,6 +26,12 @@ rm -rf ~/PawPawBuilds/builds/*/qjackctl-${QJACKCTL_VERSION}
 # ---------------------------------------------------------------------------------------------------------------------
 
 ./PawPaw/build-jack2.sh ${target}
+
+# FIXME remove this on 1.9.22
+ln -s ~/PawPawBuilds/builds/${target}/jack2-${JACK2_VERSION} PawPaw/jack2
+ln -s ~/PawPawBuilds/builds/${target}/jack2-${JACK2_VERSION} ~/PawPawBuilds/builds/${target}/jack2-v1.9.21
+export JACK2_VERSION=v1.9.21
+
 ./PawPaw/pack-jack2.sh ${target}
 
 # ---------------------------------------------------------------------------------------------------------------------
