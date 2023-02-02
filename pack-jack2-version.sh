f1cf27bbe56987838b53e34943f564f686e2548b
falkTX <falktx@falktx.com>
2023-02-02 12:07:07 +0100

    Update to jack2 1.9.22 and qjackctl 0.9.9
    Signed-off-by: falkTX <falktx@falktx.com>

--- pack-jack2-version.sh
+++ pack-jack2-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export JACK2_VERSION=1abd04edab2fef8e7b5e5197dd03034358a37078
-export QJACKCTL_VERSION=0.9.7
+export JACK2_VERSION=v1.9.22
+export QJACKCTL_VERSION=0.9.9
 
 set -e
 
@@ -26,12 +26,6 @@ rm -rf ~/PawPawBuilds/builds/*/qjackctl-${QJACKCTL_VERSION}
 # ---------------------------------------------------------------------------------------------------------------------
 
 ./PawPaw/build-jack2.sh ${target}
-
-# FIXME remove this on 1.9.22
-ln -s ~/PawPawBuilds/builds/${target}/jack2-${JACK2_VERSION} PawPaw/jack2
-ln -s ~/PawPawBuilds/builds/${target}/jack2-${JACK2_VERSION} ~/PawPawBuilds/builds/${target}/jack2-v1.9.21
-export JACK2_VERSION=v1.9.21
-
 ./PawPaw/pack-jack2.sh ${target}
 
 # ---------------------------------------------------------------------------------------------------------------------
