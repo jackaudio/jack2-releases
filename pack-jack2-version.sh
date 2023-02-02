1ac1f57b162c3189ea6eaa520dff59feeb279be5
falkTX <falktx@falktx.com>
2023-02-02 14:15:46 +0100

    Add jack-example-tools, fixup CI
    Signed-off-by: falkTX <falktx@falktx.com>

--- pack-jack2-version.sh
+++ pack-jack2-version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export JACK2_VERSION=v1.9.22
+export JACK_EXAMPLE_TOOLS_VERSION=0c4a3df94f4f87048e101f4c5ee282ed7a1080b3
 export QJACKCTL_VERSION=0.9.9
 
 set -e
@@ -21,6 +22,7 @@ fi
 
 rm -rf ~/PawPawBuilds/builds/*/jack-router-*
 rm -rf ~/PawPawBuilds/builds/*/jack2-${JACK2_VERSION}
+rm -rf ~/PawPawBuilds/builds/*/jack-example-tools-${JACK_EXAMPLE_TOOLS_VERSION}
 rm -rf ~/PawPawBuilds/builds/*/qjackctl-${QJACKCTL_VERSION}
 
 # ---------------------------------------------------------------------------------------------------------------------
